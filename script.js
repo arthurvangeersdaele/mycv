12bce987377c208994ecf3768124f8248079c899
Arthur Van Geersdaele <64006823+arthurvangeersdaele@users.noreply.github.com>
2025-06-10 20:34:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //global variables 
 let automaticScrollEnabled = true;
-let cssHost = 'https://cdn.jsdelivr.net/gh/arthurvangeersdaele/open-cv@main/';
+let cssHost = 'https://cdn.jsdelivr.net/gh/arthurvangeersdaele/open-cv@499c2d3/';
 const version = new Date().getTime(); 
 //cssHost = './'; // for dev purpose only
 
